202d27b1d0940cd65984b6c12b4873d33088db1e
EmRald Cheatum <emraldc@yahoo.com>
2023-07-31 13:25:09 -0600

    updates to calculator

--- RB100/Lesson_2/calculator.rb
+++ RB100/Lesson_2/calculator.rb
@@ -23,20 +23,20 @@ prompt("Welcome to Calculator! Enter your name:")
 
 name = ''
 loop do
-name = Kernel.gets().chomp()
+  name = Kernel.gets().chomp()
 
-if name.empty?()
-  promp("Make sure to use a valide name.")
-else
-  break
-end
+  if name.empty?()
+    promp("Make sure to use a valide name.")
+  else
+    break
+  end
 end
 
 prompt("Hi #{name}!")
 
-loop do #main loop
+loop do # main loop
   number1 = ''
-  loop do 
+  loop do
     prompt("What's the first number?")
     number1 = Kernel.gets().chomp()
 
@@ -45,32 +45,31 @@ loop do #main loop
     else
       prompt("Hmm... that doesn't look like a valid number")
     end
-    end
-
+  end
   number2 = ''
-  loop do 
+  loop do
     prompt("What's the second number?")
     number2 = Kernel.gets().chomp()
 
-  if valid_number?(number2)
-    break
-  else
-    prompt("Hmm... that doesn't look like a valid number")
-  end
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
   end
 
-operator_prompt = <<-MSG
-  What operation would you like to perform?
-  1) add
-  2) subtract
-  3) multiply
-  4) divide
-MSG
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
 
   prompt(operator_prompt)
 
   operator = ''
-  loop do 
+  loop do
     operator = Kernel.gets().chomp()
 
     if %w(1 2 3 4).include?(operator)
@@ -83,15 +82,15 @@ MSG
   prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
-  when '1'
-    number1.to_i() + number2.to_i()
-  when '2'
-    number1.to_i() - number2.to_i()
-  when '3'
-    number1.to_i() * number2.to_i()
-  when '4'
-    number1.to_f() / number2.to_f()
-  end
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() / number2.to_f()
+           end
 
   prompt("The result is #{result}")
 
